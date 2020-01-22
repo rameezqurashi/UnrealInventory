afe65870f21954879c16a903aa5c7b097261900a
Rameez Qurashi <rqurashi@dephy.com>
2020-01-22 14:03:09 -0500

    Switch to using Actor Components to be more easily Blueprintable. Inventory probably should be an actor component anyway

--- Private/Inventory.cpp
+++ Private/Inventory.cpp
@@ -3,10 +3,32 @@
 
 #include "Inventory.h"
 
-Inventory::Inventory()
+// Sets default values for this component's properties
+UInventory::UInventory()
 {
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
 }
 
-Inventory::~Inventory()
+
+// Called when the game starts
+void UInventory::BeginPlay()
 {
+	Super::BeginPlay();
+
+	// ...
+	
 }
+
+
+// Called every frame
+void UInventory::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
--- Public/Inventory.h
+++ Public/Inventory.h
@@ -3,13 +3,26 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Inventory.generated.h"
 
-/**
- * 
- */
-class INVENTORYSYSTEM_API Inventory
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class INVENTORYSYSTEM_API UInventory : public UActorComponent
 {
-public:
-	Inventory();
-	~Inventory();
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UInventory();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
 };
